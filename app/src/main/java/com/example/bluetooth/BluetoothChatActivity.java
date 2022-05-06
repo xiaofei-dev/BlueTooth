bb72b627f6387f82f72c4c4bfac9260188c7df64
xiaofei-dev <xiaofei.dev@gmail.com>
2022-05-06 23:20:03 +0800

    init

---
+++ app/src/main/java/com/example/bluetooth/BluetoothChatActivity.java
@@ -0,0 +1,256 @@
+package com.example.bluetooth;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.bluetooth.utils.ChatService;
+
+import java.util.Objects;
+
+public class BluetoothChatActivity extends AppCompatActivity {
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+    private static final int REQUEST_CONNECT_DEVICE = 1;  //请求连接设备
+    private static final int REQUEST_ENABLE_BT = 2;
+    private TextView mTitle;
+    private ListView mConversationView;
+    private EditText mOutEditText;
+    private Button mSendButton;
+    private String mConnectedDeviceName = null;
+    private ArrayAdapter<String> mConversationArrayAdapter;
+    private StringBuffer mOutStringBuffer;
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private ChatService mChatService = null;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_chat);
+        Objects.requireNonNull(getSupportActionBar()).hide();  //隐藏标题栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            }
+        }
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //创建选项菜单
+        toolbar.inflateMenu(R.menu.option_menu);
+        //选项菜单监听
+        toolbar.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        mTitle = findViewById(R.id.title_left_text);
+        mTitle.setText(R.string.app_name);
+        mTitle = findViewById(R.id.title_right_text);
+        // 得到本地蓝牙适配器
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "蓝牙不可用", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        if (!mBluetoothAdapter.isEnabled()) { //若当前设备蓝牙功能未开启
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT); //
+        } else {
+            if (mChatService == null) {
+                setupChat();  //创建会话
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length>0){
+            if(grantResults[0]!= PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "未授权，蓝牙搜索功能将不可用！", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    @Override
+    public synchronized void onResume() {
+        super.onResume();
+        if (mChatService != null) {
+            if (mChatService.getState() == ChatService.STATE_NONE) {
+                mChatService.start();
+            }
+        }
+    }
+    private void setupChat() {
+        mConversationArrayAdapter = new ArrayAdapter<String>(this, R.layout.item_chat);
+        mConversationView = findViewById(R.id.in);
+        mConversationView.setAdapter(mConversationArrayAdapter);
+        mOutEditText = findViewById(R.id.edit_text_out);
+        mOutEditText.setOnEditorActionListener(mWriteListener);
+        mSendButton = findViewById(R.id.button_send);
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView view = findViewById(R.id.edit_text_out);
+                String message = view.getText().toString();
+                sendMessage(message);
+            }
+        });
+        //创建服务对象
+        mChatService = new ChatService(this, mHandler);
+        mOutStringBuffer = new StringBuffer("");
+        ensureDiscoverable();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mChatService != null) {
+            mChatService.stop();
+        }
+        mHandler.removeCallbacksAndMessages(null);
+    }
+    private void ensureDiscoverable() { //修改本机蓝牙设备的可见性
+        //打开手机蓝牙后，能被其它蓝牙设备扫描到的时间不是永久的
+        if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            //设置在3600秒内可见（能被扫描）
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 3600);
+            startActivity(discoverableIntent);
+            Toast.makeText(this,
+                    "已经设置本机蓝牙设备的可见性，对方可搜索了。",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void sendMessage(String message) {
+        if (mChatService.getState() != ChatService.STATE_CONNECTED) {
+            Toast.makeText(this, R.string.not_connected, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (message.length() > 0) {
+            byte[] send = message.getBytes();
+            mChatService.write(send);
+            mOutStringBuffer.setLength(0);
+            mOutEditText.setText(mOutStringBuffer);
+        }
+    }
+    private TextView.OnEditorActionListener mWriteListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+            if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP) {
+                //软键盘里的回车也能发送消息
+                String message = view.getText().toString();
+                sendMessage(message);
+            }
+            return true;
+        }
+    };
+    //使用Handler对象在UI主线程与子线程之间传递消息
+    @SuppressLint("HandlerLeak")
+    private final Handler mHandler = new Handler() {   //消息处理
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case ChatService.STATE_CONNECTED:
+                            mTitle.setText(R.string.title_connected_to);
+                            mTitle.append(mConnectedDeviceName);
+                            mConversationArrayAdapter.clear();
+                            break;
+                        case ChatService.STATE_CONNECTING:
+                            mTitle.setText(R.string.title_connecting);
+                            break;
+                        case ChatService.STATE_LISTEN:
+                        case ChatService.STATE_NONE:
+                            mTitle.setText(R.string.title_not_connected);
+                            break;
+                    }
+                    break;
+                case MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    String writeMessage = new String(writeBuf);
+                    mConversationArrayAdapter.add("我:  " + writeMessage);
+                    break;
+                case MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    mConversationArrayAdapter.add(mConnectedDeviceName + ":  "
+                            + readMessage);
+                    break;
+                case MESSAGE_DEVICE_NAME:
+                    mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                    Toast.makeText(getApplicationContext(),"链接到 " + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    break;
+                case MESSAGE_TOAST:
+                    Toast.makeText(getApplicationContext(),
+                            msg.getData().getString(TOAST), Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+    //返回进入好友列表操作后的数回调方法
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE:
+                if (resultCode == Activity.RESULT_OK) {
+                    String address = data.getExtras().getString(DeviceListActiity.EXTRA_DEVICE_ADDRESS);
+                    BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+                    mChatService.connect(device);
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    Toast.makeText(this, "未选择任何好友！", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case REQUEST_ENABLE_BT:
+                if (resultCode == Activity.RESULT_OK) {
+                    setupChat();
+                } else {
+                    Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+    private class MyMenuItemClickListener implements Toolbar.OnMenuItemClickListener {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.scan:
+                    //启动DeviceList这个Activity
+                    Intent serverIntent = new Intent(BluetoothChatActivity.this, DeviceListActiity.class);
+                    startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+                    return true;
+                case R.id.discoverable:
+                    ensureDiscoverable();
+                    return true;
+                case R.id.back:
+                    finish();
+                    System.exit(0);
+                    return true;
+            }
+            return false;
+        }
+    }
+}
